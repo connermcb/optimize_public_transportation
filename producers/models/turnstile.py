333160034dcd7ea9ed27396d0097eee7917625cc
ConnerMcB <connermcb@yahoo.com>
2021-04-10 18:22:16 -0500

    create turnstyle config

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -12,14 +12,12 @@ logger = logging.getLogger(__name__)
 
 
 class Turnstile(Producer):
-    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    key_schema = avro.load(
+        f"{Path(__file__).parents[0]}/schemas/turnstile_key.json"
+    )
+    value_schema = avro.load(
+       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -30,19 +28,12 @@ class Turnstile(Producer):
             .replace("-", "_")
             .replace("'", "")
         )
-
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
         super().__init__(
-            f"{station_name}", # TODO: Come up with a better topic name
+            f"{station_name}.turnstile", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=3,
+            num_replicas=2,
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
@@ -57,3 +48,14 @@ class Turnstile(Producer):
         # of entries that were calculated
         #
         #
+        self.producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()}
+            key_schema = key_schema,
+            value={
+                "station_id"        : self.station.station_id,
+                "station_name"      : self.station.station_name,
+                "line"              : self.station.color
+            }
+            value_schema = value_schema,
+        )
