bbbacdfa710c05b74d563064ac7cc60d30c08704
ConnerMcB <connermcb@yahoo.com>
2021-04-16 09:29:24 -0500

    config logger

--- producers/connector.py
+++ producers/connector.py
@@ -4,71 +4,60 @@ import logging
 
 import requests
 
-
+# set up logging
 logger = logging.getLogger(__name__)
-
+logger.setLevel(logging.DEBUG)
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.DEBUG)
+logger.addHandler(c_handler)
 
 KAFKA_CONNECT_URL = "http://localhost:8083/connectors"
+# KAFKA_CONNECT_URL = "http://kafka-connect:8083/connectors"
+# KAFKA_CONNECT_URL = 'http://172.19.0.6'
 CONNECTOR_NAME = "stations"
 
 def configure_connector():
     """Starts and configures the Kafka Connect connector"""
-    logging.debug("creating or updating kafka connect connector...")
+    logger.debug("creating or updating kafka connect connector...")
 
     resp = requests.get(f"{KAFKA_CONNECT_URL}/{CONNECTOR_NAME}")
+
     if resp.status_code == 200:
-        logging.debug("connector already created skipping recreation")
+        logger.debug("connector already created skipping recreation")
         return
+    else:
+        logger.debug(f'Get request returned status code: {resp.status_code}')
 
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
-        return
+    logger.debug('Making connection creation request')
 
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
-    logger.info("connector code not completed skipping connector creation")
-    #resp = requests.post(
-    #    KAFKA_CONNECT_URL,
-    #    headers={"Content-Type": "application/json"},
-    #    data=json.dumps({
-    #        "name": CONNECTOR_NAME,
-    #        "config": {
-    #            "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
-    #            "key.converter": "org.apache.kafka.connect.json.JsonConverter",
-    #            "key.converter.schemas.enable": "false",
-    #            "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-    #            "value.converter.schemas.enable": "false",
-    #            "batch.max.rows": "500",
-    #            # TODO
-    #            "connection.url": "",
-    #            # TODO
-    #            "connection.user": "",
-    #            # TODO
-    #            "connection.password": "",
-    #            # TODO
-    #            "table.whitelist": "",
-    #            # TODO
-    #            "mode": "",
-    #            # TODO
-    #            "incrementing.column.name": "",
-    #            # TODO
-    #            "topic.prefix": "",
-    #            # TODO
-    #            "poll.interval.ms": "",
-    #        }
-    #    }),
-    #)
+    resp = requests.post(
+       KAFKA_CONNECT_URL,
+       headers={"Content-Type": "application/json"},
+       data=json.dumps({
+           "name": CONNECTOR_NAME,
+           "config": {
+               "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+               "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+               "key.converter.schemas.enable": "false",
+               "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+               "value.converter.schemas.enable": "false",
+               "batch.max.rows": "500",
+               "connection.url": "jdbc:postgresql://localhost:5432/cta",
+               "connection.user": "cta_admin",
+               "connection.password": "chicago",
+               "table.whitelist": "stations",
+               "mode": "incrementing",
+               "incrementing.column.name": "stop_id",
+               "topic.prefix": "cta.connector",
+               "poll.interval.ms": 3600000,
+           }
+       }),
+    )
 
     ## Ensure a healthy response was given
-    #resp.raise_for_status()
-    #logging.debug("connector created successfully")
-
+    resp.raise_for_status()
+    logger.debug("connector created successfully")
+    return
 
 if __name__ == "__main__":
     configure_connector()
