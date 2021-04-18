d44aa95884bbaafbe1980a7539368af469c438a4
ConnerMcB <connermcb@yahoo.com>
2021-04-17 20:53:51 -0500

    initial commit of configured faust api

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -33,16 +33,16 @@ class TransformedStation(faust.Record):
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic("cta.connector.stations", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic("transformed.stations", partitions=1)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+   "raw_stations",
+   default=str,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
 
 #
@@ -52,6 +52,17 @@ app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memor
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
 #
 #
+@app.agent(topic)
+async def stationevent(stationevents):
+
+
+        async for se in stationevents.group_by(Station.station_id):
+            table[se.station_id] = {
+                'station_name'  : se.station_name,
+                'order'         : se.order,
+                'line'          : [line for line in ['red','blue','green']
+                                    if se[line]][0]
+            }
 
 
 if __name__ == "__main__":
