817c620471944aa1498790b34ff323b9201f1407
ConnerMcB <connermcb@yahoo.com>
2021-05-02 16:38:28 -0500

    make connector topic name consistent between connector and consumer

--- producers/connector.py
+++ producers/connector.py
@@ -48,7 +48,7 @@ def configure_connector():
                "table.whitelist": "stations",
                "mode": "incrementing",
                "incrementing.column.name": "stop_id",
-               "topic.prefix": "cta.connector",
+               "topic.prefix": "cta.connect.",
                "poll.interval.ms": 3600000,
            }
        }),
