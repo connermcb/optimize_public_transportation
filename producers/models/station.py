b1510f801ef0aaf8da312105ca542033e627fb73
ConnerMcB <connermcb@yahoo.com>
2021-05-02 16:36:07 -0500

    consistent topic naming

--- producers/models/station.py
+++ producers/models/station.py
@@ -26,7 +26,7 @@ class Station(Producer):
             .replace("-", "_")
             .replace("'", "")
         )
-        topic_name = f"cta.stations.{station_name}" # TODO: Come up with a better topic name
+        topic_name = f"org.chicago.cta.station.{station_name}" # TODO: Come up with a better topic name
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
